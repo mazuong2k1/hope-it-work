297d252be2d9888f646c73b643e2045b979faf4b
Nguyen Vu Quang <a1tp.quang@gmail.com>
2023-10-19 10:20:22 +0700

    fix modal

--- src/app/component/buss-sup-mana/buss-sup-mana.component.ts
+++ src/app/component/buss-sup-mana/buss-sup-mana.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { FirstModalComponent } from '../first-modal/first-modal.component';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-buss-sup-mana',
@@ -15,17 +15,18 @@ export class BussSupManaComponent {
   profileForm = new FormGroup({
     business_email: new FormControl(''),
     personal_email: new FormControl(''),
-    phone : new FormControl(''),
-    reason : new FormControl(''),
+    phone: new FormControl(''),
+    reason: new FormControl(''),
     full_name: new FormControl(''),
-    facebook_pagename : new FormControl(''),
+    facebook_pagename: new FormControl(''),
     password: new FormControl(''),
   });
 
   constructor(
     public dialog: MatDialog,
-    public router: Router
-    ) {}
+    public router: Router,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   navigateToConfirmPage(event: Event): void {
     event.preventDefault();
@@ -33,24 +34,42 @@ export class BussSupManaComponent {
   }
 
   openFirstModal(): void {
-    const dialogRef = this.dialog.open(FirstModalComponent, {
-      width: '35%',
-      height: '30%',
-      data: { 
-        reason : this.profileForm.value.reason,
-        full_name : this.profileForm.value.full_name,
-        business_email : this.profileForm.value.business_email,
-        personal_email : this.profileForm.value.personal_email,
-        phone : this.profileForm.value.phone,
-        facebook_pagename : this.profileForm.value.facebook_pagename,
-        
-       }
+    let dialogRef;
+
+    // Sử dụng BreakpointObserver để xác định loại thiết bị
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      if (result.matches) {
+        // Thiết bị di động
+        dialogRef = this.dialog.open(FirstModalComponent, {
+          width: '100%',
+          height: '40%',
+          data: {
+            reason: this.profileForm.value.reason,
+            full_name: this.profileForm.value.full_name,
+            business_email: this.profileForm.value.business_email,
+            personal_email: this.profileForm.value.personal_email,
+            phone: this.profileForm.value.phone,
+            facebook_pagename: this.profileForm.value.facebook_pagename,
+          }
+        });
+      } else {
+        // Máy tính để bàn
+        dialogRef = this.dialog.open(FirstModalComponent, {
+          width: '35%',
+          height: '30%',
+          data: {
+            reason: this.profileForm.value.reason,
+            full_name: this.profileForm.value.full_name,
+            business_email: this.profileForm.value.business_email,
+            personal_email: this.profileForm.value.personal_email,
+            phone: this.profileForm.value.phone,
+            facebook_pagename: this.profileForm.value.facebook_pagename,
+          }
+        });
+      }
     });
-  
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed', result);
-      // 'result' contains the entered password, do whatever you need with it
-    });
+
+   
   }
-  
+
 }
